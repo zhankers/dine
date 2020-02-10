d99e75c104ca79630ee777beb909a62f815e0380
pan463859 <panan521@qq.com>
2020-02-10 23:17:17 +0800

    update code daily

--- wechatcode/xiaochengxu/pages/myInfo/myInfo.js
+++ wechatcode/xiaochengxu/pages/myInfo/myInfo.js
@@ -6,26 +6,30 @@ Page({
    * 页面的初始数据
    */
   data: {
-    sexs:['男','女'],
-    region:[],
-    selfinfo:{},
+    sexs: ['男', '女'],
+    region: [],
+    selfinfo: {},
     index: 0,
     customItem: '全部'
   },
-  changeSex: function (e) {
+  changeSex: function(e) {
     let temp = {}
-    temp.gender = this.data.sexs[e.detail.value]
+    temp.gender = e.detail.value
     this.setData({
-      selfinfo: { ...this.data.selfinfo, ...temp }
+      selfinfo: { ...this.data.selfinfo,
+        ...temp
+      }
     })
     this.updateUserInfo(temp)
   },
-  changeRegion: function (e) {
-    let temp={}
+  changeRegion: function(e) {
+    let temp = {}
     temp.province = e.detail.value[0]
     temp.city = e.detail.value[1]
     this.setData({
-      selfinfo: {...this.data.selfinfo, ...temp}
+      selfinfo: { ...this.data.selfinfo,
+        ...temp
+      }
     })
     this.setData({
       region: [temp.province, temp.city, '龙岗区']
@@ -35,10 +39,10 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.getUserInfo()
   },
-  updateUserInfo:function(data){
+  updateUserInfo: function(data) {
     let that = this
     let token = app._checkToken()
     wx.request({
@@ -48,28 +52,28 @@ Page({
       },
       method: "post",
       data: data,
-      success: function (response) {
+      success: function(response) {
         console.log(response)
       }
     })
   },
-  getUserInfo:function(){
-    let that=this
+  getUserInfo: function() {
+    let that = this
     let token = app._checkToken()
-      wx.request({
-        url: app.globalData.baseUrl + '/user/info',
-        header: {
-          token: token
-        },
-        success: function (response) {
-          that.setData({
-            selfinfo: response.data.data,
-            region: [response.data.data.province, response.data.data.city,'龙岗区']
-          })
-        },
-        fail: function (err) {
-          console.log(response.data.data)
-        }
-      })
-    }
+    wx.request({
+      url: app.globalData.baseUrl + '/user/info',
+      header: {
+        token: token
+      },
+      success: function(response) {
+        that.setData({
+          selfinfo: response.data.data,
+          region: [response.data.data.province, response.data.data.city, '龙岗区']
+        })
+      },
+      fail: function(err) {
+        console.log(response.data.data)
+      }
+    })
+  }
 })
